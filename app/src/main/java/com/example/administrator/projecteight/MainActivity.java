57c3fa4a68a93e9dc96d70614c597bf499e5c969
githubLins <1203624432@qq.com>
2016-09-08 09:53:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/projecteight/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.administrator.projecteight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG="MyActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG,"onCreate()");
+
+    }
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"onDestroy()");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"onPause()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG,"onRestart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"onResume()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"onStart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"onStop()");
+    }
+}
